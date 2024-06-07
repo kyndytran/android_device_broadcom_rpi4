110c20d682ea98b3737b2ef1a1f46edcf35b1454
Kha Tran <khab1304690@gmail.com>
2024-06-07 23:50:21 +0700

    rpi4: Update ARCH support for RPI4
    Only supports 64-bit arch. No second
    32-bit arch to save storage.
    
    Temporarily use generic CPU for primary ARCH
    to boot Android 14 r45.
    
    Signed-off-by: Kha Tran <khab1304690@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,11 +20,4 @@ include device/broadcom/rpi-common/BoardConfigCommon.mk
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := cortex-a72
-
-# Second Arch
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := generic
